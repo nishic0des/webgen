330ce7a20c5e90c626fe3519312da0be13b08eda
Nishi <nishi241105@gmail.com>
2026-01-19 21:47:07 +0530

    created vercel.json

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-	baseURL: "http://localhost:8000",
+	baseURL: "https://webgen-backend-s88i.onrender.com",
 });
 
 export const generateWebsite = (description) =>
